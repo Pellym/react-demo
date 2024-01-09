648b749b2b44b533c9efe65a3e78381d72b12806
Pelly <audreymphuthi78@gmail.com>
2024-01-09 14:13:06 +0200

    Additions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-export default function App(){
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.delac.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pelly Mobeng
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Pellym/react-demo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
